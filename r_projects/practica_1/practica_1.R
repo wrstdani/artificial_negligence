eb82f999c973f6f03830ec1a003524bd037130ad
Kaiser <josewalter2468@gmail.com>
2023-02-15 12:27:06 +0100

    update

--- r_projects/practica_1/practica_1.R
+++ r_projects/practica_1/practica_1.R
@@ -1,5 +1,9 @@
 install.packages("UsingR")
-paises <- read.table("paises-8.csv", header = TRUE, sep = ";")
+paises <- read.table("paises-8.csv", header = TRUE, sep = ";", stringsAsFactors = T)
+
 dimension_paises <- dim(paises)
 resumen_paises <- summary(paises)
 variables_paises <- names(paises)
+
+which(paises$Religion == "Animista")
+paises[12,]
