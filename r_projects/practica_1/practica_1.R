37c7a7891a8f12ccb54133d1bc07d31e2d6b48f2
wrstdani <ddelnuevo25@gmail.com>
2023-02-15 12:10:36 +0100

    first commit :)

---
+++ r_projects/practica_1/practica_1.R
@@ -0,0 +1,5 @@
+install.packages("UsingR")
+paises <- read.table("paises-8.csv", header = TRUE, sep = ";")
+dimension_paises <- dim(paises)
+resumen_paises <- summary(paises)
+variables_paises <- names(paises)
