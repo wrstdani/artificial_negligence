0a391ded190a6c64b912b4573ea8135fb8f25f65
Daniel del Nuevo Montero <ddelnuevo25@gmail.com>
2023-02-28 22:54:12 +0100

    Update practica_1.R

--- r_projects/practica_1/practica_1.R
+++ r_projects/practica_1/practica_1.R
@@ -1,12 +1,10 @@
 install.packages("UsingR")
 paises <- read.table("paises-8.csv", header = TRUE, sep = ";", stringsAsFactors = T)
 
-dimension_paises <- dim(paises)
-resumen_paises <- summary(paises)
-variables_paises <- names(paises)
+dim(paises)
+summary(paises)
+names(paises)
 head(paises)
-variables_paises
 
-# paises$Pais[which(paises$Religion == "Animista")]
-freq_religion <- table(paises$Religion)
-barplot(freq_religion)
+# Ejercicio 3
+barplot(paises$Urbana, col = rgb(0.92, 0.7, 0.65), main = "Porcentaje de población urbana")
