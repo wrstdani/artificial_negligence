f84a588bb3a1bc87c87f1d7f33bbd331e6e79b5e
Kaiser_AI <josewalter2468@gmail.com>
2023-03-01 16:11:35 +0100

    Update practica_1.R

--- r_projects/practica_1/practica_1.R
+++ r_projects/practica_1/practica_1.R
@@ -13,7 +13,7 @@ cor(paises.8$Esp.vida.Fem, paises.8$Alfabet, use = "complete.obs")#0.8833315
 summary(lm(paises.8$Esp.vida.Fem ~ paises.8$Alfabet))#78.03% 
 
 #Recta de Regresión;
-ggplot(paises.8, aes(Alfabet, Esp.vida.Fem)) + ggtitle("Recta de Regresión:") + xlab("Alfabetización") + ylab("Esperanza de Vida Femenina") + geom_point(colour = "pink") + geom_smooth(method = "lm", se = FALSE, colour = "black")
+ggplot(paises.8, aes(Alfabet, Esp.vida.Fem)) + ggtitle("Recta de Regresión:") + xlab("Porcentaje de Alfabetización") + ylab("Esperanza de Vida Femenina") + geom_point(colour = "pink") + geom_smooth(method = "lm", se = FALSE, colour = "black")
 
 #En este punto nos disponemos a estudiar la relación entre la esperanza de vida
 #femenina y el nivel de alfabetización de los países en la base de datos.
@@ -24,6 +24,7 @@ ggplot(paises.8, aes(Alfabet, Esp.vida.Fem)) + ggtitle("Recta de Regresión:") +
 #que están. Es tal que el 78.03% de la esperanza de vida femenina se explica 
 #mediante la alfabetización del país. Todo lo anterior queda reflejado en el siguiente
 #gráfico donde la línea roja representa la recta de regresión:
+
 #[Insertar gráfico de recta de regresión]
 
 #7R:
@@ -39,7 +40,7 @@ cor(paisesCat$Esp.vida.Fem, paisesCat$Alfabet, use = "complete.obs")#0.907876
 summary(lm(paisesCat$Esp.vida.Fem ~ paisesCat$Alfabet))#0.8242
 
 #Recta de Regresión:
-ggplot(paisesCat, aes(Alfabet, Esp.vida.Fem)) + ggtitle("Recta de Regresión de los países mayoritariamente Católicos:") + xlab("Alfabetización") + ylab("Esperanza de Vida Femenina") + geom_point(colour = "gold") + geom_smooth(method = "lm", se = FALSE, colour = "black")
+ggplot(paisesCat, aes(Alfabet, Esp.vida.Fem)) + ggtitle("Recta de Regresión de los países mayoritariamente Católicos:") + xlab("Porcentaje de Alfabetización") + ylab("Esperanza de Vida Femenina") + geom_point(colour = "gold") + geom_smooth(method = "lm", se = FALSE, colour = "black")
 
 
 #Para este apartado analizamos cual es la influencia de la religión católica
@@ -55,12 +56,18 @@ ggplot(paisesCat, aes(Alfabet, Esp.vida.Fem)) + ggtitle("Recta de Regresión de 
 #la esperanza de vida de las mujeres y la alfabetización, dicha relación es un poco
 #mayor que la observada en la muestra total donde se encuentran el resto de países.
 
+#[insertar Recta de Regresión]
+
+
+
+#Como se puede ver en el gráfico inferior los países católicos (puntos dorados) en media tienen mayor
+#porcentaje de alfabetización. Es por ello que la pendiente de su recta (recta dorada) es más inclinada 
+#llegando así a representar mejor a los valores más altos de la esperanza de vida femenina. Los puntos
+#negros representan al resto de países y la recta rosada es la recta de regresión del conjunto de todos los países.
+
 #[insertar gráfico donde se comparan con religiones y solo católica q es el código de abajo]
 
-plot(paises.8$Alfabet, paises.8$Esp.vida.Fem, main = "Comparativa entre todos los países(rosa) y los Católicos (dorada):", xlab = "Alfabetización", ylab =  "Esperanza de Vida Femenina", col = "red")
+plot(paises.8$Alfabet, paises.8$Esp.vida.Fem, main = "Comparativa:", xlab = "Porcentaje de Alfabetización", ylab =  "Esperanza de Vida Femenina", col = "black")
+points(paisesCat$Alfabet, paisesCat$Esp.vida.Fem, col = "gold")
 abline(lm(paises.8$Esp.vida.Fem ~ paises.8$Alfabet), col = "pink")
 abline(lm(paisesCat$Esp.vida.Fem ~ paisesCat$Alfabet), col = "gold")
-
-#Como se puede ver en el gráfico superior los países católicos en media tienen mayor
-#alfabetización y la pendiente de su recta es más inclinada llegando así a representar
-#mejor a los valores más altos de la esperanza de vida femenina.
